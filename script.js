b47d712ae4c81ce4ba6c7cc11b16ad339d2a9050
ayabot26 <ayabot26@gmail.com>
2026-07-21 23:58:33 +0700

    Add invitation opening interaction and music

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
+// ===============================
+// OPEN INVITATION & MUSIC
+// ===============================
 
+const openButton = document.getElementById("openInvitation");
+const music = document.getElementById("bgMusic");
+const cover = document.getElementById("cover");
+const opening = document.getElementById("opening");
+
+
+openButton.addEventListener("click", () => {
+
+
+    // Play music
+
+    music.volume = 0.5;
+
+    music.play()
+        .catch(error => {
+            console.log("Music error:", error);
+        });
+
+
+    // Hide cover
+
+    cover.style.opacity = "0";
+
+
+    setTimeout(() => {
+
+        cover.style.display = "none";
+
+        opening.scrollIntoView({
+            behavior: "smooth"
+        });
+
+    }, 1000);
+
+
+});
